0aaae970b6b5f3a48a39dd162e05fd4cc0ddd989
Fei <apitsada.s@kbtg.tech>
2021-04-22 11:47:21 +0700

    angular form binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Nonggap';
   result = "hi";
+  data = "";
+  username = "";
 
   onPress(event: any) {
     console.log(event.target.value)
     this.result = event.target.value
+  }
+
+  doSth() {
+    console.log("click")
+    this.result = "Click!!"
   }
 }
