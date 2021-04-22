c7c1378e37b926a169a205eeca07aefde838d4eb
Fei <apitsada.s@kbtg.tech>
2021-04-22 14:07:36 +0700

    add component user ,ngFor,

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor() {
+
+  }
+  setData(user: User): void {
+
+  }
 
   ngOnInit(): void {
+    this.users = [
+      new User(1, "user 01", "last 01", "1@mail.com"),
+      new User(2, "user 02", "last 02", "2@mail.com"),
+      new User(3, "user 03", "last 03", "3@mail.com"),
+    ]
   }
 
 }
