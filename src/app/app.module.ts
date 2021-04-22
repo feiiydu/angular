6c44347c85bb414f56217863d422f63232bf10bd
Fei <apitsada.s@kbtg.tech>
2021-04-22 14:27:45 +0700

    pipe my upper case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
+import { MyUpperCasePipe } from './my-upper-case.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    UserComponent
+    UserComponent,
+    MyUpperCasePipe
   ],
   imports: [
     BrowserModule,
