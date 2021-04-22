cbcd7aa8005319c8fc30e39d76acd503bcb63787
Fei <apitsada.s@kbtg.tech>
2021-04-22 18:48:25 +0700

    add color

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -10,9 +10,13 @@ export class QuizComponent implements OnInit {
   totalUnlike = 25;
   isLike =false
   isUnlike =false
+  likeButton = '';
+  dislikeButton = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.likeButton = 'unlikeCSS';
+    this.dislikeButton = 'unlikeCSS';
   }
 
   onClickLike(){
@@ -20,13 +24,16 @@ export class QuizComponent implements OnInit {
     if (this.isLike){
       this.totalLike--
       this.isLike=false
+      this.likeButton = 'unlikeCSS';
       return
     }
     this.totalLike++
     this.isLike=true
+    this.likeButton = 'likeCSS';
     if (this.isUnlike) {
       this.totalUnlike--
       this.isUnlike=false
+      this.dislikeButton = 'unlikeCSS'
     }
     console.log('like = '+ this.totalLike +this.isLike)
     console.log('unlike = '+ this.totalUnlike +this.isUnlike)
@@ -36,13 +43,16 @@ export class QuizComponent implements OnInit {
     if (this.isUnlike){
       this.totalUnlike--
       this.isUnlike=false
+      this.dislikeButton = 'unlikeCSS'
       return
     }
     this.totalUnlike++
     this.isUnlike=true
+    this.dislikeButton = 'likeCSS'
     if (this.isLike) {
       this.totalLike--
       this.isLike=false
+      this.likeButton = 'unlikeCSS';
     }
     console.log('like = '+ this.totalLike +this.isLike)
     console.log('unlike = '+ this.totalUnlike +this.isUnlike)
